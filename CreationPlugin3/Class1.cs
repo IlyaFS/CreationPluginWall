d8d0c7e430f39213c8e306d1c7696f443aba0692
Илья Филиппов <filippov.i.s@gmail.com>
2022-05-03 15:05:48 +0300

    Добавлена крыша

--- CreationPlugin3/Class1.cs
+++ CreationPlugin3/Class1.cs
@@ -70,8 +70,33 @@ namespace CreationPlugin3
             AddWindow(doc, level1, walls[1]);
             AddWindow(doc, level1, walls[2]);
             AddWindow(doc, level1, walls[3]);
+            AddExtrusionRoof(doc, level2, walls);
             transaction.Commit();
         }
+
+        private static void AddExtrusionRoof(Document doc, Level level2, List<Wall> walls)
+        {
+            LocationCurve curve = walls[1].Location as LocationCurve;
+            XYZ startPoint = curve.Curve.GetEndPoint(0);
+            XYZ endPoint = curve.Curve.GetEndPoint(1);
+
+            LocationCurve locationCurve = walls[0].Location as LocationCurve;
+            var distance = locationCurve.Curve.Length + 4;
+
+            RoofType roofType = new FilteredElementCollector(doc)
+                        .OfClass(typeof(RoofType))
+                        .OfType<RoofType>()
+                        .Where(x => x.Name.Equals("Типовой - 400мм"))
+                        .FirstOrDefault();
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ(0, startPoint.Y - 2, level2.Elevation), new XYZ(0, 0, level2.Elevation + 10)));
+            curveArray.Append(Line.CreateBound(new XYZ(0, 0, level2.Elevation + 10), new XYZ(0, endPoint.Y + 2, level2.Elevation)));
+
+            ReferencePlane refPlane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 1), new XYZ(0, 1, 0), doc.ActiveView);
+            ExtrusionRoof roof = doc.Create.NewExtrusionRoof(curveArray, refPlane, level2, roofType, -distance * 0.5, distance * 0.5);
+        }
+
         private static void AddDoor(Document doc, Level level_1, Wall wall)
         {
             FamilySymbol doorType = new FilteredElementCollector(doc)
